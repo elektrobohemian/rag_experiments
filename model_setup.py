5894b4ed02daf1b9ceab5f3075142056c369a443
David Zellhöfer <david@8bit-inferno.de>
2026-02-24 09:36:10 +0100

    fixed sentence transformer issue
    * the original model_setup.py contained some incompatible sentence transformers, e.g.:
        * x
        *
    * they have been replaced
    * commented some unnecessary debug outputs

--- model_setup.py
+++ model_setup.py
@@ -4,9 +4,12 @@
 
 # models are cached at ~/.cache/huggingface
 current_models = {
-    'SENTENCE_TRANSFORMER': 'T-Systems-onsite/cross-en-de-roberta-sentence-transformer',
-    #'SENTENCE_TRANSFORMER': 'sentence-transformers/all-MiniLM-L6-v2',
-    #'SENTENCE_TRANSFORMER': 'google/embeddinggemma-300m',
+    #'SENTENCE_TRANSFORMER': 'T-Systems-onsite/cross-en-de-roberta-sentence-transformer', # not compatible!
+    #'SENTENCE_TRANSFORMER': 'sentence-transformers/all-MiniLM-L6-v2', # all-purpose 384 dimensions model; 22.7M parameters
+    # 'SENTENCE_TRANSFORMER': 'sentence-transformers/all-mpnet-base-v2', # 768 dimensions; 0.1B parameters
+    #'SENTENCE_TRANSFORMER': 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2', # 768 dimensions; 0.3B parameters
+    'SENTENCE_TRANSFORMER':'sentence-transformers/LaBSE', # multilingual model, 0.5B parameters
+    #'SENTENCE_TRANSFORMER':'sentence-transformers/paraphrase-multilingual-mpnet-base-v2', # multilingual model, 768 dimensions; 0.3B parameters
     'LLM_ANSWER_GENERATION': 'TinyLlama/TinyLlama-1.1B-Chat-v1.0',
     #'LLM_ANSWER_GENERATION': 'meta-llama/Llama-3.2-3B-Instruct',
     #'LLM_ANSWER_GENERATION': 'meta-llama/Meta-Llama-3.1-8B-Instruct',
--- split_text.py
+++ split_text.py
@@ -15,10 +15,10 @@ def split_docs(documents, chunk_size=500, chunk_overlap=100):
         doc_parts.append(doc[0])
         file_parts.append({'file_name':doc[1]}) # we have to create a dict in order to use it as metadata below
 
-    print(f"DEBUG DOC_PARTS is of type: {type(doc_parts)}")
+    #print(f"DEBUG DOC_PARTS is of type: {type(doc_parts)}")
     documents = splitter.create_documents(doc_parts,metadatas=file_parts)
 
-    print(f"DEBUG CHUNKS is of type: {type(documents)}")
+    #print(f"DEBUG CHUNKS is of type: {type(documents)}")
 
     # debug
     #print(f"DEBUG - Generated documents:")
